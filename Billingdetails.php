59420923e9c8baccb9f46b6a7eb1541431bacb1b
Rushibhingare12 <rushibhingare12@gmail.com>
2024-04-24 14:56:42 +0530

    first commit

---
+++ Billingdetails.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['submit1']))
+{
+
+$phoneno=$_POST['phoneno'];
+$email=$_POST['email'];	
+$address=$_POST['address'];
+$sql="INSERT INTO bookingdetails(phoneno,email,address) VALUES(:phoneno,:email,:address)";
+$query = $dbh->prepare($sql);
+
+$query->bindParam(':phoneno',$phoneno,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':address',$address,PDO::PARAM_STR);
+
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Payment Successful";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> SKT | SaiKrupa Tourism </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tourism Management System In PHP" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+  <style>
+		.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+		</style>
+</head>
+<body>
+<!-- top-header -->
+<div class="top-header">
+<?php include('includes/header.php');?>
+<div class="banner-1 ">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">SKT- SaiKrupa Tourism </h1>
+	</div>
+</div>
+<!--- /banner-1 ---->
+<!--- privacy ---->
+<div class="privacy">
+	<div class="container">
+		<h3 class="wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Booking details</h3>
+		<form name="Billingdetails" method="post">
+		 <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?> 
+	
+ 
+
+
+
+<h1>Scan and Pay here.....</h1>
+
+
+	<img src="QR.jpeg <?php echo htmlentities($result->PackageImage);?>" class="img-responsive" alt="" width="300" height="300">
+
+	<p style="width: 350px;">
+
+<b>Mobile No</b>  <input type="text" name="phoneno" class="form-control" id="phoneno" maxlength="10" placeholder="10 Digit mobile No" required="">
+	</p> 
+
+	<p style="width: 350px;">
+<b>Email</b>  <input type="email" name="email" class="form-control" id="email" placeholder="Valid Email id" required="">
+	</p> 
+
+	<p style="width: 350px;">
+
+			
+			</form>
+
+		
+	</div>
+</div>
+<!--- /privacy ---->
+<!--- footer-top ---->
+<!--- /footer-top ---->
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>
+</body>
+</html>
